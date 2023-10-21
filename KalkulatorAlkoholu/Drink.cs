64073d47eafad843dda106d1cabb0be992507c14
qwert qwert <Dominikch19@gmail.com>
2023-10-21 11:29:53 +0200

    added Drink class

---
+++ KalkulatorAlkoholu/Drink.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KalkulatorAlkoholu
+{
+    class Drink
+    {
+        public Drink(String name, double alcoholConcentration) {
+            this.name = name;
+            this.alcoholConcentration = alcoholConcentration;
+        }
+        public String name
+        { get; private set; }
+
+        public double alcoholConcentration
+        { get; private set; }
+    }
+}
