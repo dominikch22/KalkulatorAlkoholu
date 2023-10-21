42c9f836ad12c8141e181c15a20da234e54af128
NoNiceQ <NoNiceq@o2.pl>
2023-10-21 19:50:48 +0200

    gui 2.0

--- KalkulatorAlkoholu/Form1.Designer.cs
+++ KalkulatorAlkoholu/Form1.Designer.cs
@@ -32,12 +32,10 @@ namespace KalkulatorAlkoholu
             this.alkoholComboBox = new System.Windows.Forms.ComboBox();
             this.capacityComboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
-            this.capacityTextBox = new System.Windows.Forms.TextBox();
             this.alkoholScoreTextBox = new System.Windows.Forms.TextBox();
             this.calculateButton = new System.Windows.Forms.Button();
             this.capacityScoreTextBox = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
@@ -57,7 +55,7 @@ namespace KalkulatorAlkoholu
             this.capacityComboBox.FormattingEnabled = true;
             this.capacityComboBox.Location = new System.Drawing.Point(32, 151);
             this.capacityComboBox.Name = "capacityComboBox";
-            this.capacityComboBox.Size = new System.Drawing.Size(125, 24);
+            this.capacityComboBox.Size = new System.Drawing.Size(260, 24);
             this.capacityComboBox.TabIndex = 1;
             this.capacityComboBox.SelectedIndexChanged += new System.EventHandler(this.capacityComboBox_SelectedIndexChanged);
             // 
@@ -71,14 +69,6 @@ namespace KalkulatorAlkoholu
             this.label1.TabIndex = 2;
             this.label1.Text = "Type of alkohol or caston percent:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
-            // 
-            // capacityTextBox
-            // 
-            this.capacityTextBox.Location = new System.Drawing.Point(167, 153);
-            this.capacityTextBox.Name = "capacityTextBox";
-            this.capacityTextBox.Size = new System.Drawing.Size(125, 22);
-            this.capacityTextBox.TabIndex = 4;
-            this.capacityTextBox.TextChanged += new System.EventHandler(this.capacityTextBox_TextChanged);
             // 
             // alkoholScoreTextBox
             // 
@@ -113,21 +103,10 @@ namespace KalkulatorAlkoholu
             this.label3.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label3.Location = new System.Drawing.Point(28, 127);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(132, 23);
+            this.label3.Size = new System.Drawing.Size(264, 23);
             this.label3.TabIndex = 10;
-            this.label3.Text = "Type of glassware:";
+            this.label3.Text = "Type of glassware or custom capacity:";
             this.label3.Click += new System.EventHandler(this.label3_Click);
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label4.Location = new System.Drawing.Point(169, 127);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(72, 23);
-            this.label4.TabIndex = 11;
-            this.label4.Text = "Capacity:";
-            this.label4.Click += new System.EventHandler(this.label4_Click);
             // 
             // label5
             // 
@@ -169,12 +148,10 @@ namespace KalkulatorAlkoholu
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label6);
             this.Controls.Add(this.label5);
-            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.capacityScoreTextBox);
             this.Controls.Add(this.calculateButton);
             this.Controls.Add(this.alkoholScoreTextBox);
-            this.Controls.Add(this.capacityTextBox);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.capacityComboBox);
             this.Controls.Add(this.alkoholComboBox);
@@ -190,12 +167,10 @@ namespace KalkulatorAlkoholu
         private System.Windows.Forms.ComboBox alkoholComboBox;
         private System.Windows.Forms.ComboBox capacityComboBox;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox capacityTextBox;
         private System.Windows.Forms.TextBox alkoholScoreTextBox;
         private System.Windows.Forms.Button calculateButton;
         private System.Windows.Forms.TextBox capacityScoreTextBox;
         private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
         private System.Windows.Forms.Label label5;
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label2;
