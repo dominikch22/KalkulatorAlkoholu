ebbbbd429f6543f65b34c50b1428ce9576f3d023
NoNiceQ <NoNiceq@o2.pl>
2023-10-22 19:33:42 +0200

    gui (błędy, ilość szkła, poprawione wymiary, dostępność, etykiety, ikona, brak możliwości zmieniania wielkości okna)

--- KalkulatorAlkoholu/Form1.Designer.cs
+++ KalkulatorAlkoholu/Form1.Designer.cs
@@ -29,6 +29,7 @@ namespace KalkulatorAlkoholu
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
             this.alkoholComboBox = new System.Windows.Forms.ComboBox();
             this.capacityComboBox = new System.Windows.Forms.ComboBox();
             this.label1 = new System.Windows.Forms.Label();
@@ -40,23 +41,29 @@ namespace KalkulatorAlkoholu
             this.label6 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.glasswareTextBox = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // alkoholComboBox
             // 
             this.alkoholComboBox.FormattingEnabled = true;
-            this.alkoholComboBox.Location = new System.Drawing.Point(32, 69);
+            this.alkoholComboBox.Location = new System.Drawing.Point(41, 52);
             this.alkoholComboBox.Name = "alkoholComboBox";
-            this.alkoholComboBox.Size = new System.Drawing.Size(260, 24);
+            this.alkoholComboBox.Size = new System.Drawing.Size(289, 24);
             this.alkoholComboBox.TabIndex = 0;
             this.alkoholComboBox.SelectedIndexChanged += new System.EventHandler(this.alkoholComboBox_SelectedIndexChanged);
             // 
             // capacityComboBox
             // 
             this.capacityComboBox.FormattingEnabled = true;
-            this.capacityComboBox.Location = new System.Drawing.Point(32, 151);
+            this.capacityComboBox.Location = new System.Drawing.Point(41, 112);
             this.capacityComboBox.Name = "capacityComboBox";
-            this.capacityComboBox.Size = new System.Drawing.Size(260, 24);
+            this.capacityComboBox.Size = new System.Drawing.Size(289, 24);
             this.capacityComboBox.TabIndex = 1;
             this.capacityComboBox.SelectedIndexChanged += new System.EventHandler(this.capacityComboBox_SelectedIndexChanged);
             // 
@@ -64,7 +71,7 @@ namespace KalkulatorAlkoholu
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label1.Location = new System.Drawing.Point(31, 43);
+            this.label1.Location = new System.Drawing.Point(37, 22);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(241, 23);
             this.label1.TabIndex = 2;
@@ -73,18 +80,20 @@ namespace KalkulatorAlkoholu
             // 
             // alkoholScoreTextBox
             // 
-            this.alkoholScoreTextBox.Location = new System.Drawing.Point(32, 289);
+            this.alkoholScoreTextBox.Location = new System.Drawing.Point(41, 284);
             this.alkoholScoreTextBox.Name = "alkoholScoreTextBox";
-            this.alkoholScoreTextBox.Size = new System.Drawing.Size(125, 22);
+            this.alkoholScoreTextBox.ReadOnly = true;
+            this.alkoholScoreTextBox.Size = new System.Drawing.Size(120, 22);
             this.alkoholScoreTextBox.TabIndex = 6;
+            this.alkoholScoreTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.alkoholScoreTextBox.TextChanged += new System.EventHandler(this.alkoholScoreTextBox_TextChanged);
             // 
             // calculateButton
             // 
-            this.calculateButton.Font = new System.Drawing.Font("Segoe Print", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.calculateButton.Location = new System.Drawing.Point(109, 216);
+            this.calculateButton.Font = new System.Drawing.Font("Segoe Print", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.calculateButton.Location = new System.Drawing.Point(122, 206);
             this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(109, 44);
+            this.calculateButton.Size = new System.Drawing.Size(125, 45);
             this.calculateButton.TabIndex = 7;
             this.calculateButton.Text = "Calculate";
             this.calculateButton.UseVisualStyleBackColor = true;
@@ -92,17 +101,19 @@ namespace KalkulatorAlkoholu
             // 
             // capacityScoreTextBox
             // 
-            this.capacityScoreTextBox.Location = new System.Drawing.Point(167, 289);
+            this.capacityScoreTextBox.Location = new System.Drawing.Point(210, 284);
             this.capacityScoreTextBox.Name = "capacityScoreTextBox";
-            this.capacityScoreTextBox.Size = new System.Drawing.Size(125, 22);
+            this.capacityScoreTextBox.ReadOnly = true;
+            this.capacityScoreTextBox.Size = new System.Drawing.Size(120, 22);
             this.capacityScoreTextBox.TabIndex = 8;
+            this.capacityScoreTextBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.capacityScoreTextBox.TextChanged += new System.EventHandler(this.capacityScoreTextBox_TextChanged);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label3.Location = new System.Drawing.Point(28, 127);
+            this.label3.Location = new System.Drawing.Point(37, 82);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(264, 23);
             this.label3.TabIndex = 10;
@@ -112,18 +123,20 @@ namespace KalkulatorAlkoholu
             // label5
             // 
             this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label5.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label5.Location = new System.Drawing.Point(298, 154);
+            this.label5.Location = new System.Drawing.Point(333, 116);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(29, 16);
+            this.label5.Size = new System.Drawing.Size(39, 20);
             this.label5.TabIndex = 12;
             this.label5.Text = "(ml)";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label6.Location = new System.Drawing.Point(31, 263);
+            this.label6.Location = new System.Drawing.Point(37, 254);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(124, 23);
             this.label6.TabIndex = 13;
@@ -134,7 +147,7 @@ namespace KalkulatorAlkoholu
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.label2.Location = new System.Drawing.Point(163, 263);
+            this.label2.Location = new System.Drawing.Point(206, 254);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(109, 23);
             this.label2.TabIndex = 14;
@@ -143,19 +156,92 @@ namespace KalkulatorAlkoholu
             // label4
             // 
             this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
             this.label4.ForeColor = System.Drawing.SystemColors.AppWorkspace;
-            this.label4.Location = new System.Drawing.Point(298, 69);
+            this.label4.Location = new System.Drawing.Point(333, 56);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(27, 16);
+            this.label4.Size = new System.Drawing.Size(36, 20);
             this.label4.TabIndex = 15;
             this.label4.Text = "(%)";
+            // 
+            // textBox1
+            // 
+            this.textBox1.AllowDrop = true;
+            this.textBox1.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.textBox1.Location = new System.Drawing.Point(60, 333);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ReadOnly = true;
+            this.textBox1.Size = new System.Drawing.Size(261, 22);
+            this.textBox1.TabIndex = 18;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.label8.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.ForeColor = System.Drawing.SystemColors.ActiveBorder;
+            this.label8.Location = new System.Drawing.Point(158, 309);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(57, 23);
+            this.label8.TabIndex = 19;
+            this.label8.Text = "Errors:";
+            // 
+            // glasswareTextBox
+            // 
+            this.glasswareTextBox.Location = new System.Drawing.Point(41, 172);
+            this.glasswareTextBox.Name = "glasswareTextBox";
+            this.glasswareTextBox.Size = new System.Drawing.Size(289, 22);
+            this.glasswareTextBox.TabIndex = 20;
+            this.glasswareTextBox.TextChanged += new System.EventHandler(this.glasswareTextBox_TextChanged);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Segoe Print", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(37, 142);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(153, 23);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Amount of glassware:";
+            this.label7.Click += new System.EventHandler(this.label7_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label9.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label9.Location = new System.Drawing.Point(162, 284);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(39, 20);
+            this.label9.TabIndex = 22;
+            this.label9.Text = "(ml)";
+            this.label9.Click += new System.EventHandler(this.label9_Click);
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label10.ForeColor = System.Drawing.SystemColors.AppWorkspace;
+            this.label10.Location = new System.Drawing.Point(333, 284);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(39, 20);
+            this.label10.TabIndex = 23;
+            this.label10.Text = "(ml)";
+            this.label10.Click += new System.EventHandler(this.label10_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonFace;
-            this.ClientSize = new System.Drawing.Size(332, 353);
+            this.ClientSize = new System.Drawing.Size(382, 378);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.glasswareTextBox);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label4);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label6);
@@ -167,6 +253,9 @@ namespace KalkulatorAlkoholu
             this.Controls.Add(this.label1);
             this.Controls.Add(this.capacityComboBox);
             this.Controls.Add(this.alkoholComboBox);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(400, 425);
+            this.MinimumSize = new System.Drawing.Size(400, 425);
             this.Name = "Form1";
             this.Text = "Kalkulator Alkoholu";
             this.ResumeLayout(false);
@@ -187,6 +276,12 @@ namespace KalkulatorAlkoholu
         private System.Windows.Forms.Label label6;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox glasswareTextBox;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
     }
 }
 
